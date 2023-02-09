753268d33f914018b7d9318ed91e9c4c024580e8
Demiaeuw <cabarbaye.adrien@gmail.com>
2023-02-09 17:18:22 +0100

    exercice terre03

--- terre03.py.py
+++ terre03.py.py
@@ -1,3 +1,4 @@
+#L’alphabet à partir de
 #Créez un programme qui affiche l’alphabet à partir de la lettre donnée en argument en lettres minuscules suivi d’un retour à la ligne.
 
 #je dois aller chercher la lettre de depart dans l'argument
@@ -9,10 +10,14 @@ import sys
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-def choix_de_lettre():
+def choisir_lettre():
     for i in range(1, len(sys.argv)):
-        print(sys.argv[i])
+        return sys.argv[i]
 
-lettre_de_depart = choix_de_lettre()
+def afficher_alphabet(lettreDepart):
+    index = alphabet.index(lettreDepart)
+    print(alphabet[index:])
 
-print(lettre_de_depart)
+if __name__ == '__main__':
+    lettre_choisie = choisir_lettre()
+    afficher_alphabet(lettre_choisie)
