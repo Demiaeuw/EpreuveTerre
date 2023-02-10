cb5bddcde0e36d264a6607e014e2ce10ffbb0f87
Demiaeuw <124376590+Demiaeuw@users.noreply.github.com>
2023-02-10 13:22:22 +0100

    fin exercice 08

---
+++ terre08.py.py
@@ -0,0 +1,31 @@
+#Puissance d’un nombre
+#Créez un programme qui affiche le résultat d’une puissance.
+#L’exposant ne pourra pas être négatif.
+
+
+
+
+import sys
+
+base = sys.argv[1]
+exposant = sys.argv[2]
+resultat = 1
+
+
+
+
+try:
+    base = int(base)
+    exposant = int(exposant)
+    resultat = int(resultat)
+
+    if exposant == 0:
+        print("erreur")
+    elif exposant < 0:
+        print("erreur")
+    else:
+        for i in range(exposant):
+            resultat *= base
+        print(resultat)
+except ValueError:
+    print("tu ne me la mettra pas val l'envers")
