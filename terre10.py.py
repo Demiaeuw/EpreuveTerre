46c0fdaf02d2d4017979a96b365839f3015db379
Demiaeuw <124376590+Demiaeuw@users.noreply.github.com>
2023-02-12 11:01:46 +0100

    Exercice 10 'les nombres premier'

---
+++ terre10.py.py
@@ -0,0 +1,29 @@
+#Nombre premier
+#Créez un programme qui affiche si un nombre est premier ou pas.
+
+#Attention : 0 et 1 ne sont pas des nombres premiers. Gérez les erreurs d’arguments.
+
+
+
+import sys
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+try:
+    number = int(sys.argv[1])
+    if is_prime(number):
+        print("C'est un nombre premier")
+    else:
+        print("Ce n'est pas un nombre premier")
+except ValueError:
+    print("Veuillez entrer un entier valide")
+
+
+
+
