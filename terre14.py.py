3ef5ea318d940c61874788b0b1139779c0f9166c
Demiaeuw <cabarbaye.adrien@gmail.com>
2023-02-13 13:47:43 +0100

    COrrestion 10 et 12 et avancé 14

--- terre14.py.py
+++ terre14.py.py
@@ -1,4 +1,32 @@
 #Trié ou pas
 #Créez un programme qui détermine si une liste d’entiers est triée ou pas.
 
-import sys 
+import sys
+
+
+
+def list_arguments(*args):
+    return [str(arg) for arg in args]
+
+args = [int(arg) for arg in sys.argv[1:]]
+listeDarguments = list_arguments(*args)
+
+def nombre_premier(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+
+try:
+    listeDarguments = int(listeDarguments)
+    if nombre_premier(listeDarguments) and listeDarguments == listeDarguments.sort():
+        print("Triée !")
+    elif nombre_premier(listeDarguments) and listeDarguments != listeDarguments.sort():
+        print("Pas triée !")
+    else:
+        print("erreur")
+except ValueError:
+    print("Entré une liste de nombre premier separé par des espaces")
