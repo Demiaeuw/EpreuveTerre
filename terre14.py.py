7c105803f4d89b29f1fa29806732aab9e9893c6e
Demiaeuw <cabarbaye.adrien@gmail.com>
2023-02-13 16:05:23 +0100

    Test si nombre premier + affiche les non premiers

--- terre14.py.py
+++ terre14.py.py
@@ -12,8 +12,8 @@
 
 import sys
 
-
-def list_arguments(*args):                                                                              #recuperer la liste d'argument 
+#mes fonctions 
+"""def list_arguments(*args):                                                                              #recuperer la liste d'argument 
     return [str(arg) for arg in args]
 
 def nombre_premier(n):                                                                                  #definir si c'est un nombre premier
@@ -27,11 +27,11 @@ def nombre_premier(n):                                                          
 
 args = [int(arg) for arg in sys.argv[1:]]
 listeDarguments = list_arguments(*args)
+listeNombrePremier = nombre_premier(listeDarguments)"""
 
 
-
-
-try:
+#1ere version 
+"""try:
 
     if nombre_premier(listeDarguments) and listeDarguments == listeDarguments.sort():                   #savoir si c'est une liste en ordre
         print("Triée !")
@@ -40,4 +40,42 @@ try:
     else:
         print("erreur")
 except ValueError:                                                                                      #gerer les erreurs 
-    print("Entré une liste de nombre premier separé par des espaces")
+    print("Entré une liste de nombre premier separé par des espaces")"""
+
+
+#2eme version
+"""try:
+    if all(listeNombrePremier[i] <= listeNombrePremier[i+1] for i in range(len(listeNombrePremier)-1)):
+        print("Triée !")
+    else:
+        print("Pas triée !")
+except ValueError:
+    print("Erreur")"""
+
+
+def nombre_premier(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+arguments = sys.argv[1:]
+nombrePremier = []
+nombrePasPremier = []
+
+
+for argument in arguments:
+    try:
+        nombre = int(argument)
+        if nombre_premier(nombre):
+            nombrePremier.append(argument)
+        else:
+            nombrePasPremier.append(argument)
+    except ValueError:
+        nombrePasPremier.append(argument)
+
+if nombrePasPremier:
+    non_valides = ", ".join(nombrePasPremier)
+    print(f"'{non_valides}', ne sont pas des entiers premiers !")
