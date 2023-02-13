f9c7dce11c4556aa720c86d08258bd950ce97f21
Demiaeuw <cabarbaye.adrien@gmail.com>
2023-02-13 14:30:44 +0100

    Exercice 14 ne fonctionne pas

--- terre14.py.py
+++ terre14.py.py
@@ -1,17 +1,22 @@
 #Trié ou pas
 #Créez un programme qui détermine si une liste d’entiers est triée ou pas.
 
+
+
+#recuperer la liste d'argument 
+#definir si c'est un nombre premier
+#savoir si c'est une liste en ordre
+#gerer les erreurs 
+
+
+
 import sys
 
 
-
-def list_arguments(*args):
+def list_arguments(*args):                                                                              #recuperer la liste d'argument 
     return [str(arg) for arg in args]
 
-args = [int(arg) for arg in sys.argv[1:]]
-listeDarguments = list_arguments(*args)
-
-def nombre_premier(n):
+def nombre_premier(n):                                                                                  #definir si c'est un nombre premier
     if n <= 1:
         return False
     for i in range(2, n):
@@ -20,13 +25,19 @@ def nombre_premier(n):
     return True
 
 
+args = [int(arg) for arg in sys.argv[1:]]
+listeDarguments = list_arguments(*args)
+
+
+
+
 try:
-    listeDarguments = int(listeDarguments)
-    if nombre_premier(listeDarguments) and listeDarguments == listeDarguments.sort():
+
+    if nombre_premier(listeDarguments) and listeDarguments == listeDarguments.sort():                   #savoir si c'est une liste en ordre
         print("Triée !")
     elif nombre_premier(listeDarguments) and listeDarguments != listeDarguments.sort():
         print("Pas triée !")
     else:
         print("erreur")
-except ValueError:
+except ValueError:                                                                                      #gerer les erreurs 
     print("Entré une liste de nombre premier separé par des espaces")
