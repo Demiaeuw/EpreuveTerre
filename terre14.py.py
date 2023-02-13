d2beb434582cce5941bc7c923c3f4d3200b354a3
Demiaeuw <cabarbaye.adrien@gmail.com>
2023-02-13 16:27:45 +0100

    validation

--- terre14.py.py
+++ terre14.py.py
@@ -12,45 +12,6 @@
 
 import sys
 
-#mes fonctions 
-"""def list_arguments(*args):                                                                              #recuperer la liste d'argument 
-    return [str(arg) for arg in args]
-
-def nombre_premier(n):                                                                                  #definir si c'est un nombre premier
-    if n <= 1:
-        return False
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
-
-
-args = [int(arg) for arg in sys.argv[1:]]
-listeDarguments = list_arguments(*args)
-listeNombrePremier = nombre_premier(listeDarguments)"""
-
-
-#1ere version 
-"""try:
-
-    if nombre_premier(listeDarguments) and listeDarguments == listeDarguments.sort():                   #savoir si c'est une liste en ordre
-        print("Triée !")
-    elif nombre_premier(listeDarguments) and listeDarguments != listeDarguments.sort():
-        print("Pas triée !")
-    else:
-        print("erreur")
-except ValueError:                                                                                      #gerer les erreurs 
-    print("Entré une liste de nombre premier separé par des espaces")"""
-
-
-#2eme version
-"""try:
-    if all(listeNombrePremier[i] <= listeNombrePremier[i+1] for i in range(len(listeNombrePremier)-1)):
-        print("Triée !")
-    else:
-        print("Pas triée !")
-except ValueError:
-    print("Erreur")"""
 
 
 def nombre_premier(n):
@@ -62,6 +23,7 @@ def nombre_premier(n):
     return True
 
 arguments = sys.argv[1:]
+
 nombrePremier = []
 nombrePasPremier = []
 
